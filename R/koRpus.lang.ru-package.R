e9291687a4f6f6de402fe64dcdca894bd19fb48f
unDocUMeantIt <m@reaktanz.de>
2018-01-05 01:24:59 +0100

    debianized package for first release

--- R/koRpus.lang.ru-package.R
+++ R/koRpus.lang.ru-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab koRpus.lang.ru\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.1-1\cr
-#' Date: \tab 2018-01-03\cr
+#' Date: \tab 2018-01-05\cr
 #' Depends: \tab R (>= 3.1),koRpus (>= 0.11-2)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
