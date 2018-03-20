7099dd5cd6502f9e3f07eeab26838f21edbf4b36
unDocUMeantIt <m@reaktanz.de>
2018-03-20 15:21:33 +0100

    fixed incomplete package name in license info

--- R/lang.support-ru.R
+++ R/lang.support-ru.R
@@ -1,19 +1,19 @@
 # Copyright 2010-2017 Meik Michalke <meik.michalke@hhu.de>
 #
-# This file is part of the R package koRpus.
+# This file is part of the R package koRpus.lang.ru.
 #
-# koRpus is free software: you can redistribute it and/or modify
+# koRpus.lang.ru is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# koRpus is distributed in the hope that it will be useful,
+# koRpus.lang.ru is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with koRpus.  If not, see <http://www.gnu.org/licenses/>.
+# along with koRpus.lang.ru.  If not, see <http://www.gnu.org/licenses/>.
 
 
 # this is an internal file providing language support.
